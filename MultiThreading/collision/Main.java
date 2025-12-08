e8c1c54ed5ccbf186b56800bba2abfe84dd560b1
Louis2324 <miguellouis2023@gmail.com>
2025-12-08 20:23:55 +0200

    Deleted redundant files

--- MultiThreading/collision/Main.java
+++
@@ -1,5 +0,0 @@
-package MultiThreading.collision;
-
-public class Main {
-    public static int c = 0;
-}
--- MultiThreading/collision/Person.java
+++
@@ -1,7 +0,0 @@
-package MultiThreading.collision;
-
-public class Person {
-    public void run() {
-        Main.c = 5;
-    }
-}
