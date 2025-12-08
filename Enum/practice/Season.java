c6f2e4feb4bb20b04304f6a9d4f5ffd30804205f
Louis2324 <miguellouis2023@gmail.com>
2025-12-08 19:14:35 +0200

    Other practice file

--- Enum/practice/Season.java
+++ Enum/practice/Season.java
@@ -1,4 +1,19 @@
 package Enum.practice;
 
 public enum Season {
+    WINTER("cold"),
+    SUMMER("hot"),
+    SPRING("Pleasant"),
+    FALL("Cool");
+
+    private String description;
+
+    Season(String descrp) {
+        this.description = descrp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
--- Enum/practice/SeasonMain.java
+++ Enum/practice/SeasonMain.java
@@ -1,4 +1,10 @@
 package Enum.practice;
 
 public class SeasonMain {
+    public static void main(String[] args) {
+
+        for (Season s : Season.values()) {
+            System.out.println(s.name().substring(0, 1).toUpperCase() + s.name().substring(1).toLowerCase() + " is " + s.getDescription());
+        }
+    }
 }
